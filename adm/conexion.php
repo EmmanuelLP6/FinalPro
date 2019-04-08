6565da369fd2e54e1a32378d622f113319a7a384
Esteban Jiménez Ruiz <estebanj659@gmail.com>
2019-04-08 11:07:31 -0500

    correcion de conexion

--- adm/conexion.php
+++ adm/conexion.php
@@ -1,5 +1,4 @@
 <?php
-  $conexion = mysqli_connect("localhost","estebanJ","12345","pueblosmagicos") or die("Error al conectarse con el servidor");
-  // $conexion = mysqli_connect("localhost","user_pueblo_magico","usuariopueblomagico123","pueblosmagicos") or die("Error al conectarse con el servidor");
+  $conexion = mysqli_connect("localhost","user_pueblo_magico","usuariopueblomagico123","pueblosmagicos") or die("Error al conectarse con el servidor");
   mysqli_query($conexion,"SET NAMES 'utf8'");
 ?>
