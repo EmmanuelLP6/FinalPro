f5c5639cf70cca2ef9269773849a0bce616d9776
Etianee <44274195+Etianee@users.noreply.github.com>
2019-04-08 05:48:15 -0500

    sdfg

--- portal/pueblos-magicos.php
+++ portal/pueblos-magicos.php
@@ -77,7 +77,7 @@
 				<?php
 					
 				?>
-        <section class="dark-wrapper opaqued parallax" data-parallax="scroll" data-image-src="assets/img/bg/bg2.jpg" data-speed="0.7">
+        <section class="dark-wrapper opaqued parallax" data-parallax="scroll" data-image-src="../recursos-portal/imagenes/header-pueblo.jpg" data-speed="0.7">
             <div class="section-inner pad-top-200">
                 <div class="container">
                     <div class="row">
