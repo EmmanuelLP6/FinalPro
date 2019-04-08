49232ad2a4f3475eb9ad47581643a12800107e34
Liz Salazar <lizflores858@gmail.com>
2019-04-08 09:24:22 -0500

    Create pueblosmagicos1.sql
    Base de Datos correcta

---
+++ recursos/pueblosmagicos1.sql
@@ -0,0 +1,284 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-04-2019 a las 16:18:40
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.1.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pueblosmagicos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artesania`
+--
+
+CREATE TABLE `artesania` (
+  `id_artesania` int(11) NOT NULL,
+  `nombre_artesania` varchar(60) NOT NULL,
+  `materiales` text NOT NULL,
+  `significado` text NOT NULL,
+  `img_artesania` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `id_estado` int(11) NOT NULL,
+  `nombre_estado` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `localidad`
+--
+
+CREATE TABLE `localidad` (
+  `id_localidad` int(11) NOT NULL,
+  `nombre_localidad` varchar(60) NOT NULL,
+  `id_municipio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `municipio`
+--
+
+CREATE TABLE `municipio` (
+  `id_municipio` int(11) NOT NULL,
+  `nombre_municipio` varchar(60) NOT NULL,
+  `id_estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `platillo`
+--
+
+CREATE TABLE `platillo` (
+  `id_platillo` int(11) NOT NULL,
+  `nombre_platillo` varchar(60) NOT NULL,
+  `preparacion` text,
+  `imagen_platillo` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pueblo`
+--
+
+CREATE TABLE `pueblo` (
+  `id_pueblo` int(11) NOT NULL,
+  `nombre_pueblo` varchar(60) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `estatus` int(1) NOT NULL,
+  `img1` text NOT NULL,
+  `img2` text NOT NULL,
+  `img3` text NOT NULL,
+  `id_platillo` int(11) NOT NULL,
+  `id_artesania` int(11) NOT NULL,
+  `id_localidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `razon`
+--
+
+CREATE TABLE `razon` (
+  `id_razon` int(11) NOT NULL,
+  `descripcion` text NOT NULL,
+  `id_pueblo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(5) NOT NULL,
+  `nombre_usuario` varchar(30) NOT NULL,
+  `ape_paterno` varchar(30) NOT NULL,
+  `ape_materno` varchar(30) NOT NULL,
+  `correo_electronico` varchar(40) NOT NULL,
+  `password` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `ape_paterno`, `ape_materno`, `correo_electronico`, `password`) VALUES
+(1, 'Administrador', 'Pueblo', 'Mágico', 'administrador@pueblomagico.com', '0521c86db1b2fc3d0a9e333ffda2d277a14cf20924f033afff3e0cb4f15928c2');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `artesania`
+--
+ALTER TABLE `artesania`
+  ADD PRIMARY KEY (`id_artesania`);
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`id_estado`);
+
+--
+-- Indices de la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  ADD PRIMARY KEY (`id_localidad`),
+  ADD KEY `id_municipio` (`id_municipio`);
+
+--
+-- Indices de la tabla `municipio`
+--
+ALTER TABLE `municipio`
+  ADD PRIMARY KEY (`id_municipio`),
+  ADD KEY `id_estado` (`id_estado`);
+
+--
+-- Indices de la tabla `platillo`
+--
+ALTER TABLE `platillo`
+  ADD PRIMARY KEY (`id_platillo`);
+
+--
+-- Indices de la tabla `pueblo`
+--
+ALTER TABLE `pueblo`
+  ADD PRIMARY KEY (`id_pueblo`),
+  ADD KEY `id_localidad` (`id_localidad`),
+  ADD KEY `id_artesania` (`id_artesania`),
+  ADD KEY `id_platillo` (`id_platillo`);
+
+--
+-- Indices de la tabla `razon`
+--
+ALTER TABLE `razon`
+  ADD PRIMARY KEY (`id_razon`),
+  ADD KEY `id_pueblo` (`id_pueblo`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `artesania`
+--
+ALTER TABLE `artesania`
+  MODIFY `id_artesania` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `id_estado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  MODIFY `id_localidad` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `municipio`
+--
+ALTER TABLE `municipio`
+  MODIFY `id_municipio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `platillo`
+--
+ALTER TABLE `platillo`
+  MODIFY `id_platillo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pueblo`
+--
+ALTER TABLE `pueblo`
+  MODIFY `id_pueblo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `razon`
+--
+ALTER TABLE `razon`
+  MODIFY `id_razon` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  ADD CONSTRAINT `localidad_ibfk_1` FOREIGN KEY (`id_municipio`) REFERENCES `municipio` (`id_municipio`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `municipio`
+--
+ALTER TABLE `municipio`
+  ADD CONSTRAINT `municipio_ibfk_1` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `pueblo`
+--
+ALTER TABLE `pueblo`
+  ADD CONSTRAINT `pueblo_ibfk_1` FOREIGN KEY (`id_localidad`) REFERENCES `localidad` (`id_localidad`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pueblo_ibfk_2` FOREIGN KEY (`id_artesania`) REFERENCES `artesania` (`id_artesania`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pueblo_ibfk_3` FOREIGN KEY (`id_platillo`) REFERENCES `platillo` (`id_platillo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `razon`
+--
+ALTER TABLE `razon`
+  ADD CONSTRAINT `razon_ibfk_1` FOREIGN KEY (`id_pueblo`) REFERENCES `pueblo` (`id_pueblo`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
